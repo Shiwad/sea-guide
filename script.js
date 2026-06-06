d2c73829779072b97ce9f7ea657f1188e1245475
Gerald Bual <166917962+Shiwad@users.noreply.github.com>
2026-06-06 21:20:24 +0800

    Initial commit: Tagum City sea guide promotional website

---
+++ script.js
@@ -0,0 +1,50 @@
+function openMenu(){
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+
+        else{
+            reveals[i].classList.remove('active');
+        }
+
+    }
+}
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset
+
+    if(currentScroll <= 0){
+        body.classList.remove("scroll-up")
+    }
+
+    if(currentScroll > lastScroll && !body.classList.contains("scroll-down")){
+        body.classList.remove("scroll-up")
+        body.classList.add("scroll-down")
+    }
+
+    if(currentScroll < lastScroll && body.classList.contains("scroll-down")){
+        body.classList.remove("scroll-down")
+        body.classList.add("scroll-up")
+    }
+
+    lastScroll = currentScroll;
+})
